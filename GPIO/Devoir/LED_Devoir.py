8a835a9a913d90b00252985325556aec7b7486c2
hepl-DegrezArthur <arthur.degrez@student.hepl.be>
2024-09-20 17:01:15 +0200

    Add files via upload

---
+++ GPIO/Devoir/LED_Devoir.py
@@ -0,0 +1,58 @@
+# Librairies
+import machine
+import utime
+
+# Définition
+LED = machine.Pin(18, machine.Pin.OUT)
+Button = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_DOWN)
+
+# Variables
+count = 0
+i=1
+
+# Fonction d'interruption
+def button_handler(pin):
+    global i
+    i += 1
+    if i==6:
+        i=1
+
+# Configuration de l'interruption sur changement d'état
+Button.irq(trigger=machine.Pin.IRQ_RISING, handler=button_handler)
+
+while True:
+    while i==1:
+        LED.toggle()
+        utime.sleep(0.2)
+    while i==2:
+        LED.toggle()
+        utime.sleep(0.5)
+    while i==3:
+        LED.toggle()
+        utime.sleep(0.5)
+        LED.toggle()
+        utime.sleep(0.1)
+        LED.toggle()
+        utime.sleep(0.5)
+    while i==4:
+        LED.toggle()
+        utime.sleep(0.5)
+        for y in range (10):
+            LED.toggle()
+            utime.sleep(0.1)
+        LED.toggle()
+        utime.sleep(0.5)
+    while i==5:
+        LED.value(0)
+
+"""
+# Boucle principale
+while True:
+    if i!=1:
+        LED.toggle()
+        utime.sleep(0.1*i*2)
+    
+    else:
+        LED.value(0)
+"""
+        
